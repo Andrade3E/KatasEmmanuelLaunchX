295a19fc804c28e3de21b596a02afc0af466216f
Emmanuel Andrade <86005425+Andrade3E@users.noreply.github.com>
2022-02-14 09:12:27 -0600

    Archivos Katas Emmanuel

---
+++ Kata del modulo 1/python.py
@@ -0,0 +1,24 @@
+sum = 1 + 2 # 3   #Documentacion
+product = sum * 2
+print(product)
+
+distancia_a_alfa_centauri = 4.367
+type(distancia_a_alfa_centauri)
+
+print(type)
+from datetime import date
+date.today()
+print(date.today())
+
+print("Today's date is: " + str(date.today()))
+
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(first_number + second_number)
+
+print(int(first_number) + int(second_number))
